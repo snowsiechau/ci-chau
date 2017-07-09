780f6556915503b55709a451a02015a3e47a70c1
snow_siechau <chautho95@gmail.com>
2017-07-09 22:32:51 +0700

    Added background, back buffer redering, gameloop

--- src/Program.java
+++ src/Program.java
@@ -1,8 +1,17 @@
+import game.GameWindow;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Vector;
+
 /**
- * Created by huynq on 7/4/17.
+ * Created by SNOW on 7/4/2017.
  */
 public class Program {
     public static void main(String[] args) {
-
+        GameWindow gameWindow = new GameWindow();
+        gameWindow.run();
     }
+
 }
